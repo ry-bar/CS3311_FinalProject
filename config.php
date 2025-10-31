e07388606396f01abca5bf110800e3e56779b289
Ralph Liam McHugh <59300910+Figureofstick@users.noreply.github.com>
2025-10-31 13:44:56 -0600

    Php starting files

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "scrap_database";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if($conn->connect_error){
+    die("Failed to connect to DB:".$conn->connect_error);
+}
+
+?>
